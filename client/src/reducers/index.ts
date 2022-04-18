ddb1fd064a07bf9df33a025bf30848165d56a1be
Sergio Patiño <sergio.patino@productividapp.com>
2022-04-17 21:02:21 -0500

    Removed reducer

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import messages from './messages';
 import users from './users';
-import user from './user';
 
 const reducers = combineReducers({
   messages,
-  users,
-  user
+  users
 });
 
 export default reducers;
