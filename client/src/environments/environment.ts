8cc1f94e3d70689c8076539e1f4db8cb84dbbc0e
Sergio Patiño <sergio.patino@productividapp.com>
2022-04-17 22:56:47 -0500

    Added env to client

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment: any = {
   webSocket: {
-    socketEndpoint: process.env.REACT_APP_SOCKET_ENDPOINT || 'http://localhost:4000'
+    socketEndpoint: process.env.REACT_APP_SOCKET_ENDPOINT
   },
   giphy: {
-    apiURL: process.env.REACT_APP_GIPHY_API_URL || 'https://api.giphy.com/v1/gifs/search',
-    token: process.env.REACT_APP_GIPHY_API_TOKEN || 'zo417xUsT9h8Fj3465qnUGEPOdVk8JlV'
+    apiURL: process.env.REACT_APP_GIPHY_API_URL,
+    token: process.env.REACT_APP_GIPHY_API_TOKEN
   }
 };
